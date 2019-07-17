cbb24021acf7459fa97953595381bee195c1c3f8
wondergo2017 <wondergo2017@gmail.com>
2019-07-17 10:54:14 +0800

    add BN & Dropout

--- model/model/DeepFM.py
+++ model/model/DeepFM.py
@@ -15,15 +15,24 @@ class DeepFM(nn.Module):
         self.field_size = config.getint('model', 'hidden_size')
         self.field_num= 9
         self.field_encoder = FieldEncoder(config)
+        self.deep_layer1=config.getint('model','deep_layer1')
+        self.deep_layer2=config.getint('model','deep_layer2')
+        self.dropout=nn.Dropout()
         self.mlp=nn.Sequential(
-                    nn.Linear(self.field_num*self.field_size,256),
+                    nn.Linear(self.field_num*self.field_size,self.deep_layer1),
+                    nn.BatchNorm1d(self.deep_layer1),
                     nn.ReLU(),
-                    nn.Linear(256,128),
-                    nn.ReLU()
+                    nn.Dropout(),
+                    nn.Linear(self.deep_layer1,self.deep_layer2),
+                    nn.BatchNorm1d(self.deep_layer2),
+                    nn.ReLU(),
+                    nn.Dropout()
                 )
-        self.final=nn.Linear(self.field_size+self.field_num+128,2)
+        self.final=nn.Linear(self.field_size+self.field_num+self.deep_layer2,2)
+        
     def init_multi_gpu(self, device):
         self.field_encoder = nn.DataParallel(self.field_encoder)
+        self.mlp=nn.DataParallel(self.mlp)
         pass
 
     def forward(self, data, criterion, config, usegpu, acc_result = None):
@@ -35,14 +44,16 @@ class DeepFM(nn.Module):
         # # -----------FM part------------------------------
         # ------------first order term---------
         self.y_first_order= torch.sum(self.emb,dim=2)
-        
+        self.y_first_order= self.dropout(self.y_first_order)
         # ---------- second order term ---------------
         summed=torch.sum(self.emb,dim=1)
         self.summed_square=torch.mul(summed,summed) #None *K
         self.squared_sum=torch.sum(torch.mul(self.emb,self.emb),dim=1) #None *K
         self.y_second_order=0.5*(self.summed_square-self.squared_sum) # None*K
+        self.y_second_order=self.dropout(self.y_second_order)
         # # -----------DNN part----------------------------
         self.y_deep=self.emb.reshape((-1,self.field_num*self.field_size))
+        self.y_deep=self.dropout(self.y_deep)
         self.y_deep=self.mlp(self.y_deep)
         self.output=torch.cat([self.y_first_order,self.y_second_order,self.y_deep],dim=1)
         self.output=self.final(self.output)
