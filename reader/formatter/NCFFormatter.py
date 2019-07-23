265e4904929fe9703e38727733722534782a2ef2
wondergo2017 <wondergo2017@gmail.com>
2019-07-23 14:17:34 +0800

    refine to final data

--- reader/formatter/NCFFormatter.py
+++ reader/formatter/NCFFormatter.py
@@ -19,33 +19,54 @@ class NCFFormatter:
         userid = int(data['user'])
         if self.user.check(userid, config) is None:
             return None
-
-        musicId= list(data['music'].keys())
-        musicId=[id1 for id1 in musicId if not(self.music.check(id1,config) is None)]
-        musicNum=len(musicId)
-        musicNgNum=int(musicNum*self.ng)
-        if musicNum <1 :
-            return None
-        musicNGid=[]
-        musicNgCnt=0
-        while musicNgCnt<musicNgNum:
-            id1=random.sample(self.allmusic,1)[0]
-            if self.music.check(id1,config) is None:
-                continue    
-            musicNgCnt+=1
-            musicNGid.append(id1)
-        musicId.extend(musicNGid)
-        labels= [1 for _ in range(musicNum)]+[0 for _ in range(int(musicNgNum))]
-        return [[userid for _ in range(len(labels))],musicId,labels]
+#         testId= data['music']
+#         if self.music.check(testId,config) is None:
+#             return None
+#         testLabel=data['label']
+#         musicId= list(data['history'].keys())
+#         musicId=[id1 for id1 in musicId if not(self.music.check(id1,config) is None)]
+#         musicNum=len(musicId)
+#         musicNgNum=int(musicNum*self.ng)
+#         if musicNum <1 :
+#             return None
+#         musicNGid=[]
+#         musicNgCnt=0
+#         while musicNgCnt<musicNgNum:
+#             id1=random.sample(self.allmusic,1)[0]
+#             if self.music.check(id1,config) is None:
+#                 continue    
+#             musicNgCnt+=1
+#             musicNGid.append(id1)
+#         musicId.extend(musicNGid)
+#         labels= [1 for _ in range(musicNum)]+[0 for _ in range(int(musicNgNum))]
+#         return [[userid for _ in range(len(labels))],musicId,labels,testId,testLabel]
+        
+        testId=data['music']
+        testLabel=data['label']
+        return [userid,testId,testLabel]
 
     def format(self, alldata, config, transformer, mode):
         labels  =[]
         musicids=[]
         userids =[]
+#         for d in alldata:
+#             labels.extend(d[2])
+#             musicids.extend(d[1])
+#             userids.extend(d[0])
+#         if mode=='train':
+#             for d in alldata:
+#                 labels.extend(d[2])
+#                 musicids.extend(d[1])
+#                 userids.extend(d[0])   
+#         elif mode=='valid':
+#             for d in alldata:
+#                 labels.append(d[4])
+#                 musicids.append(d[3])
+#                 userids.append(d[0][0])
         for d in alldata:
-            labels.extend(d[2])
-            musicids.extend(d[1])
-            userids.extend(d[0])
+            labels.append(d[2])
+            musicids.append(d[1])
+            userids.append(d[0])
         music = []
         users = []
         for id1 in musicids:
